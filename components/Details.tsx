a2ae70ac50bbf3e3eda77ace26b4aa526fc1b220
Sushmitha S <94210498+Sushmitha-SK@users.noreply.github.com>
2022-11-16 12:27:20 +0530

    Updated

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,31 +1,21 @@
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Image,Linking } from 'react-native'
 import React from 'react'
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { Feather } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
-
-
-
-
+import { Button } from 'react-native-paper';
 
 const Details = ({ route }: any) => {
   const { recipe } = route.params;
-
-
   return (
-
     <ScrollView>
 
       <View style={styles.details}>
         <View style={styles.subcontainer}>
           <Image style={styles.recipeimage}
-            // source={{ uri: `${recipe.image}` }}
             source={{ uri: `${recipe.image}` }}
-
           />
           <Text style={styles.recipetitle}>
-            {/* {`${recipe.label}`} */}
             {`${recipe.label}`}
           </Text>
 
@@ -54,14 +44,18 @@ const Details = ({ route }: any) => {
             <Text style={styles.ingredientsdata}>{`${recipe.ingredients.map((item: any) => item['food'])}`}</Text>
         </View>
 
-      
-
         <View style={styles.preparedish}>
           <Text style={styles.titletext}>Prepare your Dish</Text>
           <Text style={styles.preparedishdata}>{`${recipe.ingredientLines}`}</Text>
         </View>
 
-
+        
+        <Button style={styles.button} uppercase={false}
+               onPress={() => {
+                Linking.openURL(`${recipe.url}`);
+              }}>
+                <Text style={styles.btntext}>Get Instructions</Text>
+            </Button>
 
       </View>
     </ScrollView>
@@ -71,22 +65,18 @@ const Details = ({ route }: any) => {
 const styles = StyleSheet.create({
   details: {
     marginBottom: 30,
-    // padding: 5,
   },
 
   recipeimage: {
     width: '100%',
     height: 200,
-    // borderRadius: 20
   },
+
   subcontainer: {
     paddingBottom: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#cbd4cf'
-
   },
-
-
 
   recipetitle: {
     fontSize: 22,
@@ -95,10 +85,7 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     marginTop: 2,
     padding: 5
-
   },
-
-
 
   detailtab: {
     flex: 1,
@@ -124,14 +111,11 @@ const styles = StyleSheet.create({
     color: '#5bd29a',
     textTransform: 'capitalize',
     padding: 5
-
   },
 
   ingredients: {
     margin: 10,
-   //flexDirection: 'row',
    flex:1,
-   // flexWrap: 'wrap',
     paddingBottom: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#cbd4cf'
@@ -141,8 +125,8 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 18,
     color: '#363a38',
-
   },
+
   ingredientsdata:{
     fontSize: 16,
     color: '#363a38',
@@ -150,17 +134,10 @@ const styles = StyleSheet.create({
     paddingTop:5,
     flex: 1, 
     flexWrap: 'wrap',
-    
-   
-
   },
- 
-
  
   preparedish:{
     margin: 10,
-    // flexDirection: 'row',
-   //  flexWrap: 'wrap',
      paddingBottom: 10,
      borderBottomWidth: 1,
      borderBottomColor: '#cbd4cf'
@@ -171,9 +148,34 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     fontSize: 16,
     color: '#363a38',
-  }
+  },
+  
+  button: {
+    backgroundColor: '#5bd29a',
+    width: 327,
+    height: 54,
+    padding: 5,
+    borderWidth: 2,
+    borderRadius: 16,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    shadowColor: '#171717',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 2,
+},
 
-
+btntext: {
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 16,
+    display: 'flex',
+    alignItems: 'center',
+    color: 'white',
+},
 
 })
 
